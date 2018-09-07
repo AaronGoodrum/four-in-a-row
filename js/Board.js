231dff2a435638a75a928bd54e1da822c994e4d4
AaronGoodrum <ag505181@gmail.com>
2018-09-06 17:32:04 -0700

    building token and player

---
+++ js/Board.js
@@ -0,0 +1,14 @@
+/**
+ *
+ The constructor method should define the following properties: rows, columns, and spaces.
+ Set the value of the rows property to 6.
+ Set the value of the columns property to 7.
+ Set the value of the spaces property to an empty array.
+
+ */
+
+ class Board {
+   constructor(){
+     this.rows = 6;
+   }
+ }
--- js/Token.js
+++ js/Token.js
@@ -1,6 +1,9 @@
 class Token {
   constructor(index, owner) {
+    //properties of a token are dependent on the player that owns it.
     this.owner = owner;
+
+    //each token object of a given player will now have a unique ID
     this.id = `token-${index}-${owner.id}`;
     this.dropped = false;
   }
